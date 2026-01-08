c588399f24802dfe9baafc1f39acf97a419643ab
nahiyankhan55 <kushbuladesignmax+++@gmail.com>
2026-01-08 19:23:52 +0600

    problem 4

--- script.js
+++ script.js
@@ -32,3 +32,15 @@ function isPalindrome(str) {
 }
 console.log(isPalindrome("madam"));
 console.log(isPalindrome("hello"));
+
+// Problem 4: Find the Maximum Number
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+console.log(findMax([5, 1, 9, 3]));
