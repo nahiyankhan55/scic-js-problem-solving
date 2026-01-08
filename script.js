e04aebd84b895ba2a966f4db3565684a89eceaed
nahiyankhan55 <kushbuladesignmax+++@gmail.com>
2026-01-08 19:15:49 +0600

    problem 2

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function reverseString(str) {
   return result;
 }
 console.log(reverseString("hello"));
+
+// Problem 2: Count Vowels in a String
+function countVowels(str) {
+  let vowels = "aeiou";
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      count = count + 1;
+    }
+  }
+  return count;
+}
+console.log(countVowels("programming"));
