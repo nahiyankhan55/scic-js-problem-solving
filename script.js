57ff1e68c8250237c7621b6ee207c5f259f0f540
nahiyankhan55 <kushbuladesignmax+++@gmail.com>
2026-01-08 19:20:10 +0600

    problem 3

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ function countVowels(str) {
   return count;
 }
 console.log(countVowels("programming"));
+
+// Problem 3: Check for Palindrome
+function isPalindrome(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return str === reversed;
+}
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("hello"));
